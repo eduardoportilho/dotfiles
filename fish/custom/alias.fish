c3ab4d9d675080ec96de11e677c6a7e5f851821c
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2016-04-21 15:22:40 +0200

    cclear

--- fish/custom/alias.fish
+++ fish/custom/alias.fish
@@ -5,23 +5,39 @@
 
 set message "Aliases:"
 
-# perfect ls
+################################
+# Perfect ls
+
 function ll
     ls -la $argv
 end
 set message $message" ll"
 
+################################
+# website - cache clear
+
+alias cclear "sudo rm -rf ~/src/website/Symfony/app/cache/dev/; and sudo chmod -R a+w ~/src/website/Symfony/app/cache"
+set message $message" cclear"
+
 if [ $OS = "Linux" ]
+
+    ################################
     # Fast(er) devify
+
     alias devf "/src/website/devify.sh --auto --skip-static --skip-npm --skip-language --skip-cms --setup-permissions"
     set message $message", devf"
+
 end
 
 if [ $OS = "OSX" ]
-    # show and hide hidden files on Finder
+
+    ################################
+    # Show and hide hidden files on Finder
+
     alias findershow 'defaults write com.apple.finder AppleShowAllFiles YES ; killall Finder'
     alias finderhide 'defaults write com.apple.finder AppleShowAllFiles NO ; killall Finder'
     set message $message", findershow, finderhide"
+
 end
 
 echo $message
